f9411cc3fb064c8c849bb013a6f12fb197f9c0e8
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2023-11-03 20:32:08 +0200

    Refactor code formatting in TripsTraps.test.js

--- src/TripsTraps.test.js
+++ src/TripsTraps.test.js
@@ -1,51 +1,45 @@
-import React from 'react';
-import { render, fireEvent, screen} from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
 
-
-
-test('clicking on a square updates the board', () => {
+test("clicking on a square updates the board", () => {
   render(<App />);
-  const squareElement = screen.getByTestId('square-0');
+  const squareElement = screen.getByTestId("square-0");
   fireEvent.click(squareElement);
   expect(screen.getByTestId("square-0")).toHaveTextContent("X");
   //const statusElement = getByText(/Next player/i);
   //expect(statusElement).toHaveTextContent(/O/i);
-
 });
 
-test('2 x vajutus vasakult paremale, tulemuseks tuleb x ja o', () => {
+test("2 x vajutus vasakult paremale, tulemuseks tuleb x ja o", () => {
   render(<App />);
-  const ruut1 = screen.getByTestId('square-0');
+  const ruut1 = screen.getByTestId("square-0");
   fireEvent.click(ruut1);
   expect(screen.getByTestId("square-0")).toHaveTextContent("X");
 
-  const ruut2 = screen.getByTestId('square-1');
+  const ruut2 = screen.getByTestId("square-1");
   fireEvent.click(ruut2);
   expect(screen.getByTestId("square-1")).toHaveTextContent("O");
-
 });
 
-test('2 x vajutus esimesele ruudule, peab jääma alles X', () => {
+test("2 x vajutus esimesele ruudule, peab jääma alles X", () => {
   render(<App />);
-  const ruut1 = screen.getByTestId('square-0');
+  const ruut1 = screen.getByTestId("square-0");
   fireEvent.click(ruut1);
   fireEvent.click(ruut1);
   expect(screen.getByTestId("square-0")).toHaveTextContent("X");
-
 });
 
-test('7 sammu võiduni', () => {
+test("7 sammu võiduni", () => {
   render(<App />);
-  const ruut0 = screen.getByTestId('square-0');
-  const ruut1 = screen.getByTestId('square-1');
-  const ruut2 = screen.getByTestId('square-2');
-  const ruut3 = screen.getByTestId('square-3');
-  const ruut4 = screen.getByTestId('square-4');
-  const ruut5 = screen.getByTestId('square-5');
-  const ruut6 = screen.getByTestId('square-6');
-  
-  
+  const ruut0 = screen.getByTestId("square-0");
+  const ruut1 = screen.getByTestId("square-1");
+  const ruut2 = screen.getByTestId("square-2");
+  const ruut3 = screen.getByTestId("square-3");
+  const ruut4 = screen.getByTestId("square-4");
+  const ruut5 = screen.getByTestId("square-5");
+  const ruut6 = screen.getByTestId("square-6");
+
   fireEvent.click(ruut0);
   fireEvent.click(ruut1);
   fireEvent.click(ruut2);
@@ -53,26 +47,23 @@ test('7 sammu võiduni', () => {
   fireEvent.click(ruut4);
   fireEvent.click(ruut5);
   fireEvent.click(ruut6);
-  
 
-  const statusElement = screen.getByTestId('status');
+  const statusElement = screen.getByTestId("status");
   expect(statusElement).toHaveTextContent(/Winner: X/i);
-
 });
 
-
-test('kontrollime staatus teadet ja mängu', () => {
+test("kontrollime staatus teadet ja mängu", () => {
   render(<App />);
-  const ruut0 = screen.getByTestId('square-0');
-  const ruut1 = screen.getByTestId('square-1');
-  const ruut2 = screen.getByTestId('square-2');
-  const ruut3 = screen.getByTestId('square-3');
-  const ruut4 = screen.getByTestId('square-4');
-  const ruut5 = screen.getByTestId('square-5');
-  const ruut6 = screen.getByTestId('square-6');
-  
+  const ruut0 = screen.getByTestId("square-0");
+  const ruut1 = screen.getByTestId("square-1");
+  const ruut2 = screen.getByTestId("square-2");
+  const ruut3 = screen.getByTestId("square-3");
+  const ruut4 = screen.getByTestId("square-4");
+  const ruut5 = screen.getByTestId("square-5");
+  const ruut6 = screen.getByTestId("square-6");
+
   fireEvent.click(ruut0);
-  expect(screen.getByTestId("square-0")).toHaveTextContent("X"); 
+  expect(screen.getByTestId("square-0")).toHaveTextContent("X");
   expect(screen.getByTestId("status")).toHaveTextContent("Next player: O");
   fireEvent.click(ruut1);
   expect(screen.getByTestId("square-1")).toHaveTextContent("O");
@@ -92,9 +83,4 @@ test('kontrollime staatus teadet ja mängu', () => {
   fireEvent.click(ruut6);
   expect(screen.getByTestId("square-6")).toHaveTextContent("X");
   expect(screen.getByTestId("status")).toHaveTextContent("Winner: X");
-  
-
-  
-  
-
 });
