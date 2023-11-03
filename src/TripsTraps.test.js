3b43a2985f9bcbdb0552de90512b7ffcd476f032
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2023-11-03 19:55:53 +0200

    Add tests for winning and game status

--- src/TripsTraps.test.js
+++ src/TripsTraps.test.js
@@ -35,3 +35,66 @@ test('2 x vajutus esimesele ruudule, peab jääma alles X', () => {
 
 });
 
+test('7 sammu võiduni', () => {
+  render(<App />);
+  const ruut0 = screen.getByTestId('square-0');
+  const ruut1 = screen.getByTestId('square-1');
+  const ruut2 = screen.getByTestId('square-2');
+  const ruut3 = screen.getByTestId('square-3');
+  const ruut4 = screen.getByTestId('square-4');
+  const ruut5 = screen.getByTestId('square-5');
+  const ruut6 = screen.getByTestId('square-6');
+  
+  
+  fireEvent.click(ruut0);
+  fireEvent.click(ruut1);
+  fireEvent.click(ruut2);
+  fireEvent.click(ruut3);
+  fireEvent.click(ruut4);
+  fireEvent.click(ruut5);
+  fireEvent.click(ruut6);
+  
+
+  const statusElement = screen.getByTestId('status');
+  expect(statusElement).toHaveTextContent(/Winner: X/i);
+
+});
+
+
+test('kontrollime staatus teadet ja mängu', () => {
+  render(<App />);
+  const ruut0 = screen.getByTestId('square-0');
+  const ruut1 = screen.getByTestId('square-1');
+  const ruut2 = screen.getByTestId('square-2');
+  const ruut3 = screen.getByTestId('square-3');
+  const ruut4 = screen.getByTestId('square-4');
+  const ruut5 = screen.getByTestId('square-5');
+  const ruut6 = screen.getByTestId('square-6');
+  
+  fireEvent.click(ruut0);
+  expect(screen.getByTestId("square-0")).toHaveTextContent("X"); 
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: O");
+  fireEvent.click(ruut1);
+  expect(screen.getByTestId("square-1")).toHaveTextContent("O");
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: X");
+  fireEvent.click(ruut2);
+  expect(screen.getByTestId("square-2")).toHaveTextContent("X");
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: O");
+  fireEvent.click(ruut3);
+  expect(screen.getByTestId("square-3")).toHaveTextContent("O");
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: X");
+  fireEvent.click(ruut4);
+  expect(screen.getByTestId("square-4")).toHaveTextContent("X");
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: O");
+  fireEvent.click(ruut5);
+  expect(screen.getByTestId("square-5")).toHaveTextContent("O");
+  expect(screen.getByTestId("status")).toHaveTextContent("Next player: X");
+  fireEvent.click(ruut6);
+  expect(screen.getByTestId("square-6")).toHaveTextContent("X");
+  expect(screen.getByTestId("status")).toHaveTextContent("Winner: X");
+  
+
+  
+  
+
+});
