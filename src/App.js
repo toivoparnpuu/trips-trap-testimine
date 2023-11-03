e9b8b880352abea9bc8ce8f045b6f626a246ab0f
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2023-11-03 20:44:12 +0200

    Add links to React and testing library tutorials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div>
       <h1>Trips traps trull</h1>
+      <p>Mäng järgib <a href="https://react.dev/learn/tutorial-tic-tac-toe"> Reacti õpetust</a> </p>
+      <p>Testimise osas on <a href="https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#not-using-screen">kasutatud Kent C. Dodds blogi juhiseid</a></p>
       <Game />
     </div>
   );
